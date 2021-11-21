b69615349e252319f91506429c482f47643a7849
ivymmurithi <ivymurithi@gmail.com>
2021-11-21 16:58:40 +0300

    add hover effect for design column

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#design').hover(
+        function() {
+            $('#design').text('Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing. Throughout your project, our designers create and implement visual design and workflows, solicit user feedback and work with you to make sure what gets built is what is needed.');
+        }
+    )
+});
