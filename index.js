9cb294b7fb6a628a262f79e859c19294df217ad4
ivymmurithi <ivymurithi@gmail.com>
2021-11-21 21:02:13 +0300

    delete of previous validation process

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
+// Hover
 $(document).ready(function(){
-    $('#design').hover(
+    $('#design').mouseenter(
         function() {
-            $('#design').text('Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing. Throughout your project, our designers create and implement visual design and workflows, solicit user feedback and work with you to make sure what gets built is what is needed.');
+            $('#design').text('Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing. Throughout your project, our designers create and implement visual design and workflows, solicit user feedback and work with you to make sure what gets built is what is needed.')
         }
     )
 
-    $('#development').hover(
+    $('#development').mouseenter(
         function() {
-            $('#development').text('All engineers are fluent in the latest enterprise, mobile and web development technologies. They collaborate with your team to write, and improve code on a daily basis, using proven practices such as test-driven development and pair programming.');
+            $('#development').text('All engineers are fluent in the latest enterprise, mobile and web development technologies. They collaborate with your team to write, and improve code on a daily basis, using proven practices such as test-driven development and pair programming.')
         }
     )
 
-    $('#project-mngt').hover(
+    $('#projectmngt').mouseenter(
         function() {
-            $('project-mngt').text('Planning and development is iterative. Because we are constantly coding and testing, the products we build are always ready to go live. This iterative process allows for changes as business requirements evolve.')
+            $('projectmngt').text('Planning and development is iterative. Because we are constantly coding and testing, the products we build are always ready to go live. This iterative process allows for changes as business requirements evolve.')
         }
     )
 
+});
+
+
+$('#submit').submit(function(event) {
+    event.preventDefault();
+    let fname = $('#fname').val();
+    let email = $('#email').val();
+    let message = $('#message').val();
+    if (fname === "Yo") {
+    alert("Fill in the form");
+    }
 });
