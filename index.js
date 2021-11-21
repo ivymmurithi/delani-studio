9c24f2a264cf06ac3fec6c17ea11f9ebe1362204
ivymmurithi <ivymurithi@gmail.com>
2021-11-21 17:03:42 +0300

    add development hover and text

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
             $('#design').text('Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing. Throughout your project, our designers create and implement visual design and workflows, solicit user feedback and work with you to make sure what gets built is what is needed.');
         }
     )
+
+    $('#development').hover(
+        function() {
+            $('#development').text('All engineers are fluent in the latest enterprise, mobile and web development technologies. They collaborate with your team to write, and improve code on a daily basis, using proven practices such as test-driven development and pair programming.');
+        }
+    )
+
+    
+
 });
